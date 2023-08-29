55333dca299bf39f0c31131a1311ec32a2696f46
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-08-29 02:08:18 -0300

    fix: cross-origin link element attribute error on importing cdn resource

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,11 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" cross-origin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          cross-origin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
           rel="stylesheet"
