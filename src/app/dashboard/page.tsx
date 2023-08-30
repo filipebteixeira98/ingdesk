96232cc461897b6f6ace5ad5a52d4a91cded3522
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-08-30 16:36:10 -0300

    chore: created dashboard page client component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Header } from '@/components/Header'
+import { Sidebar } from '@/components/Sidebar'
+
+export default function Dashboard() {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+    </>
+  )
+}
