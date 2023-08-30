5095c25c858e79dc887e89cea3c135467618bb05
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-08-30 16:28:48 -0300

    refactor: header/sidebar components according seo/syntax & design patterns/principles good practices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,43 +13,59 @@ import logoImg from 'public/logo-ingcorp.svg'
 
 export function Header() {
   return (
-    <header className="max-w-[1440px] h-[72px] bg-white">
+    <header className="max-w-[1440px] h-[72px] bg-white" data-aos="fade-down">
       <nav className="w-full h-full p-3 flex items-center justify-between">
         <div className="flex items-center justify-center">
-          <button className="mr-8">
+          <button type="button" className="mr-8">
             <List className="w-6 h-6 text-[#3e3e51]" />
           </button>
-          <a href="#" className="mr-6">
-            <Image src={logoImg} alt="logo image" width={127} height={51} />
-          </a>
-          <button className="flex items-center justify-center gap-3">
+          <button type="button" className="mr-6">
+            <Image
+              className="w-auto h-auto"
+              src={logoImg}
+              alt="logo image"
+              width={127}
+              height={51}
+              priority
+            />
+          </button>
+          <button
+            type="button"
+            className="flex items-center justify-center gap-3"
+          >
             <UserCircle className="w-8 h-8 text-[#d9dae1]" />
             <span className="text-sm text-[#3e3f44]">Lucas Romero Cardim</span>
           </button>
         </div>
         <div className="flex items-center justify-center">
           <span className="text-xs text-[#3e3f44]">Grupo: Prospecção</span>
-          <button className="w-[168px] h-[50px] ml-[112px] flex items-center justify-start rounded-md p-3 bg-[#45c777]">
+          <button
+            type="button"
+            className="w-[168px] h-[50px] ml-[112px] flex items-center justify-start rounded-md p-3 bg-[#45c777]"
+          >
             <PhoneCall className="w-[28px] h-[30px] text-white mr-2" />
             <div className="flex flex-col text-start">
               <span className="text-sm text-white">Livre</span>
               <span className="text-[10px] text-white">00:00:00</span>
             </div>
           </button>
-          <button className="w-[134px] h-9 flex items-center justify-between p-2 bg-white border-2 border-[#788693] rounded-md ml-2">
+          <button
+            type="button"
+            className="w-[134px] h-9 flex items-center justify-between p-2 bg-white border-2 border-[#788693] rounded-md ml-2"
+          >
             <span className="text-sm text-[#bcbfd0]">Pausas</span>
             <CaretRight className="text-lg text-[#bcbfd0]" />
           </button>
         </div>
         <div className="w-[168px] h-full flex items-center justify-center">
           <div className="flex gap-6">
-            <button>
+            <button type="button">
               <Gear className="w-6 h-6 text-[#828292]" />
             </button>
-            <button>
+            <button type="button">
               <Bell className="w-6 h-6 text-[#828292]" />
             </button>
-            <button>
+            <button type="button">
               <SignOut className="w-6 h-6 text-[#828292]" />
             </button>
           </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -14,42 +14,48 @@ import {
 
 export function Sidebar() {
   return (
-    <aside className="max-h-screen w-16">
+    <aside className="max-h-screen w-16" data-aos="fade-right">
       <div className="w-full h-full flex flex-col items-center justify-between p-3 gap-20">
         <div className="flex flex-col gap-8">
-          <button>
+          <button type="button">
             <Gauge className="w-6 h-6 text-[#828292]" />
           </button>
-          <button>
+          <button type="button">
             <Handshake className="w-6 h-6 text-[#828292]" />
           </button>
-          <button>
+          <button type="button">
             <UsersThree className="w-6 h-6 text-[#828292]" />
           </button>
-          <button>
+          <button type="button">
             <UserGear className="w-6 h-6 text-[#828292]" />
           </button>
-          <button>
+          <button type="button">
             <ChatCircleDots className="w-6 h-6 text-[#828292]" />
           </button>
-          <button>
+          <button type="button">
             <MagnifyingGlass className="w-6 h-6 text-[#828292]" />
           </button>
-          <button>
+          <button type="button">
             <ProjectorScreenChart className="w-6 h-6 text-[#828292]" />
           </button>
-          <button>
+          <button type="button">
             <CheckSquare className="w-6 h-6 text-[#828292]" />
           </button>
-          <button>
+          <button type="button">
             <ClipboardText className="w-6 h-6 text-[#828292]" />
           </button>
         </div>
         <div className="flex flex-col gap-4">
-          <button className="w-[40px] h-[40px] p-[10px] rounded-[87px] bg-white border border-[#828292] border-opacity-10 grid place-content-center">
+          <button
+            type="button"
+            className="w-[40px] h-[40px] p-[10px] rounded-[87px] bg-white border border-[#828292] border-opacity-10 grid place-content-center"
+          >
             <Plus className="text-sm text-[#828292]" />
           </button>
-          <button className="w-[40px] h-[40px] p-[10px] rounded-[87px] bg-[#005daa] grid place-content-center">
+          <button
+            type="button"
+            className="w-[40px] h-[40px] p-[10px] rounded-[87px] bg-[#005daa] grid place-content-center"
+          >
             <Phone className="text-[22px] text-white" />
           </button>
         </div>
